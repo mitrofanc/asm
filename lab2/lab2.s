bf7d99abbc8484e2d94b2a796bb41b6d60829417
mitrofanc <mitrofancc@yandex.ru>
2025-04-04 00:02:38 +0300

    finish lab2

--- lab2/lab2.s
+++ lab2/lab2.s
@@ -1,20 +1,30 @@
 bits 64
 
 section .data
-n:   dd 8                 ; Количество 8-битных элементов
-mas: db 8, 7, 1, 9, 6, 0, 4, 3  ; 8 элементов по 1 байту
+rows:	dd 3
+columns:dd 8         
+matrix: db 8, 7, 121, -9, 126, 123, 4, -120  
+		db 100, 110, -3, 1, 9, -99, 90, 111
+		db 121, 0, -99, 80, -23, 0, 56, 70
 
 section .text
 global _start
 
 _start:
-    mov rbx, mas          ; rbx указывает на начало массива
-    mov r11d, [n]         ; r11d = число элементов (n)
-    mov r8d, r11d         ; r8d = gap (изначально равен n)
-    mov r9d, 1            ; r9d = swapped, ставим 1, чтобы гарантировать вход в цикл
+	mov rbx, matrix
+	mov r14d, [rows]
+	mov r15d, 0			; текущая строка 
 
+row_loop:
+	cmp r15d, r14d		; текущая строка >= число строк
+	jae end
+
+	mov r11d, [columns]
+	mov r8d, r11d		; gap (изначально = числу элементов)
+	mov r9d, 1 			; swapped 
+	
 comb_sort_loop:
-    ; Пересчитываем gap, если он больше 1
+    ; пересчитываем gap, если он больше 1
     cmp r8d, 1
     jle .gap_update_skip
     mov eax, r8d
@@ -28,50 +38,50 @@ comb_sort_loop:
 .set_gap_new:
     mov r8d, eax
 .gap_update_skip:
-    ; Ставим swapped = 0 (пока не произошло обмена)
+    ; swapped = 0 (пока не произошло перестановки)
     xor r9d, r9d
 
-    ; Внутренний цикл: сравниваем элементы, отстоящие на gap
-    mov ecx, r11d         ; ecx = n
-    sub ecx, r8d          ; ecx = n - gap
+    ; внутренний цикл: сравниваем элементы, отстоящие на gap друг от друга
+    mov ecx, r11d         ; ecx = columns
+    sub ecx, r8d          ; ecx = columns - gap (количество сравнений для данного gap)
     mov rdi, 0            ; rdi = i = 0
 
 .inner_loop:
     cmp rdi, rcx
     jge .comb_loop_end
 
-    ; Адрес элемента (i + gap) считаем через промежуточный регистр
+    ; адрес элемента (i + gap)
     mov r10, rdi
     add r10d, r8d         ; r10d = i + gap
 
-    ; Загружаем байты в dl и bl
-    ; В 64-битной адресации нельзя одновременно использовать rdi*scale + r8*scale
-    ; поэтому делаем сложение в r10d, а затем обращаемся к [rbx + r10].
+    ; загружаем значения в r12b и r13b
     mov rax, rdi
-    mov r12b, [rbx + rax]   ; dl = mas[i]
-    mov r13b, [rbx + r10]   ; bl = mas[i + gap]
-    ;mov r12b, [rbx + rdi]
-    ;mov r13b, [rbx + rdi + r8d]
+    mov r12b, [rbx + rax]   ; r12b = mas[i]
+    mov r13b, [rbx + r10]   ; r13b = mas[i + gap]
 
-    ; Сравниваем r12b и r13b
+    ; сравниваем r12b и r13b
     cmp r12b, r13b
     jle .no_swap
-    ; Если dl > bl, меняем их местами
+    ; если r12b > r13b, меняем их местами
     mov [rbx + rax], r13b
     mov [rbx + r10], r12b
-    mov r9d, 1            ; swapped = 1
+    mov r9d, 1              ; swapped = 1
 .no_swap:
     inc rdi
     jmp .inner_loop
 
 .comb_loop_end:
-    ; Продолжаем, если gap > 1 или если были обмены
+    ; продолжаем, если gap > 1 или если были обмены
     cmp r8d, 1
     jg comb_sort_loop
     cmp r9d, 1
     je comb_sort_loop
 
-    ; Выходим из программы
-    mov eax, 60           ; sys_exit
+    add rbx, r11		; сдвиг строки на следующую
+    inc r15d
+    jmp row_loop
+
+end:
+    mov eax, 60         
     xor edi, edi
     syscall
